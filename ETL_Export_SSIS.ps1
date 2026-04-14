2a997bc39aba4ee4cbed10b698de65c8b6b59431
PabloMorixe <45852748+PabloMorixe@users.noreply.github.com>
2026-04-14 19:05:00 -0300

    Create ETL_Export_SSIS.ps1

---
+++ ETL_Export_SSIS.ps1
@@ -0,0 +1,39 @@
+
+--exporta por folder
+--2 EJECUTAR LA SALIDA DE ESTA QUERY EN UN CMD
+
+
+WITH FOLDERS AS
+(
+    SELECT
+        CAST('\'+PF.foldername AS VARCHAR(MAX)) AS SSISPath,
+        CAST('E:\SSIS_EXPORT\' + PF.foldername AS VARCHAR(MAX)) AS FilePath,
+        PF.folderid,
+        PF.parentfolderid
+    FROM msdb.dbo.sysssispackagefolders PF
+    WHERE PF.parentfolderid IS NULL
+
+    UNION ALL
+
+    SELECT
+        CAST(F.SSISPath + '\' + PF.foldername AS VARCHAR(MAX)) AS SSISPath,
+        CAST(F.FilePath + '\' + PF.foldername AS VARCHAR(MAX)) AS FilePath,
+        PF.folderid,
+        PF.parentfolderid
+    FROM msdb.dbo.sysssispackagefolders PF
+    JOIN FOLDERS F ON PF.parentfolderid = F.folderid
+),
+PACKAGES AS
+(
+    SELECT
+        name AS PackageName,
+        folderid
+    FROM msdb.dbo.sysssispackages
+)
+SELECT
+    'dtutil /sourceserver ' + @@SERVERNAME +
+    ' /SQL "' + F.SSISPath + '\' + P.PackageName +
+    '" /copy file;"' + F.FilePath + '\' + P.PackageName + '.dtsx"' AS cmd
+FROM FOLDERS F
+JOIN PACKAGES P ON P.folderid = F.folderid
+WHERE F.SSISPath <> '\Data Collector';
